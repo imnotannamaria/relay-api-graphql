4d5ab2f8e2e215ce201f85059fba5ed35169ce04
Anna Maria <aannamariabr@outlook.com>
2022-09-24 11:16:05 -0300

    Criando um GraphQLObjectType(model) para posts

---
+++ src/graphql/post/postType.ts
@@ -0,0 +1,31 @@
+import { GraphQLObjectType, GraphQLString } from "graphql";
+import { connectionDefinitions, globalIdField } from "graphql-relay";
+
+// Basicamente um Model
+
+const PostType = new GraphQLObjectType({
+  name: 'Post',
+  description: 'Post Type',
+  fields: () => ({
+    id: globalIdField('Post'),
+    title: {
+      type: GraphQLString,
+      resolve: post => post.title,
+    },
+    body: {
+      type: GraphQLString,
+      resolve: post => post.body,
+    }
+  })
+})
+
+const { connectionType: PostConnection, edgeType: PostEdge } = connectionDefinitions({
+  nodeType: PostType
+})
+
+export {
+  PostConnection,
+  PostEdge,
+}
+
+export default PostType;
