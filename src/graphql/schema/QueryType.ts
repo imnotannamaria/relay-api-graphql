b548970d014b5d9a617c6b2c121d3390875f693e
Anna Maria <aannamariabr@outlook.com>
2022-09-24 11:32:14 -0300

    Criando um QueryType para concentrar todas as querys da aplicação(entry point)

---
+++ src/graphql/schema/QueryType.ts
@@ -0,0 +1,23 @@
+// Concentra todas as query da aplicação (entry point)
+
+import { GraphQLNonNull, GraphQLObjectType } from "graphql";
+import { connectionArgs, connectionFromArray } from "graphql-relay";
+import { PostConnection } from "../post/postType";
+
+import * as PostLoader from "../../graphql/post/mutations/PostLoader";
+
+const QueryType = new GraphQLObjectType({
+  name: 'Query',
+  description: 'The root of all queries',
+  fields: () => ({
+    posts: {
+      type: new GraphQLNonNull(PostConnection),
+      args: connectionArgs,
+      //first, after, before, last
+      resolve: async (_, args, context) => {
+        const data = await PostLoader.loadAll();
+        return connectionFromArray(data, args);
+      }
+    }
+  })
+});
